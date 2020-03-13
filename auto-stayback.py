476a1f1ad812cbc8a3b5a45f2261f82dea435a77
gokul <gokulkrh@gmail.com>
2020-03-13 12:07:22 +0530

    applies for given names

--- auto-stayback.py
+++ auto-stayback.py
@@ -4,55 +4,66 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from datetime import datetime
 
-EMAILFIELD = (By.ID, "i0116")
-PASSWORDFIELD = (By.ID, "i0118")
-NEXTBUTTON = (By.ID, "idSIButton9")
-button = (By.ID, "idSIButton9")
-submit = (By.NAME, "login")
+n = []
+c = []
+present = []
+with open('/home/gokul/Desktop/cred.txt') as input:
+    for line in input:
+        c.append(line.rstrip())
+with open('/home/gokul/Desktop/attendancelog.txt') as input:
+    for line in input:
+        e = line.replace(" ", "")
+        n.append(e.rstrip())
+for i in c:
+    o = i.split(" ; ")
+    if o[0] in n:
+        present.append(i)
 
-browser = webdriver.Chrome()
-browser.get('https://my.amrita.ac.in/index/index')
+for i in present:
+    i = i.split(" ; ")
+    email = (By.ID, "i0116")
+    password = (By.ID, "i0118")
+    next = (By.ID, "idSIButton9")
+    button = (By.ID, "idSIButton9")
+    signin = (By.NAME, "login")
 
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(submit)).click()
+    browser = webdriver.Chrome()
+    browser.get('https://my.amrita.ac.in/index/index') # opens my amrita homepage
 
-# wait for email field and enter email
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(EMAILFIELD)).send_keys("your username")
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(signin)).click() # waits for the submit button and clicks
 
-# Click Next
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(NEXTBUTTON)).click()
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(email)).send_keys(i[1]) # wait for email field and enter email
 
-# wait for password field and enter password
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(PASSWORDFIELD)).send_keys("your password")
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(next)).click() # click next
 
-# Click Login - same id?
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(NEXTBUTTON)).click()
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(button)).click()
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(password)).send_keys(i[2]) # wait for password field and enter password
 
-browser.get('https://my.amrita.ac.in/index/index?key=585356-bfrth6gfv44k875-5jghtgf58ddfrtt&mode=in')
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(next)).click() # Click next
 
-stayback = (By.XPATH, '//*[@id="content"]/div[2]/div/div/div[6]/div/h2/a')
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(stayback)).click()
+    browser.get('https://my.amrita.ac.in/index/index?key=585356-bfrth6gfv44k875-5jghtgf58ddfrtt&mode=in') # Once logged in opens AHMS homepage
 
-purpose = (By.XPATH, '//*[@id="normal_stayback"]/form/fieldset/section[1]/div/label[2]/i')
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(purpose)).click()
+    stayback = (By.XPATH, '//*[@id="content"]/div[2]/div/div/div[6]/div/h2/a') # clicks on the stayback option
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(stayback)).click()
 
-approval_from = (By.XPATH, '//*[@id="staff_id"]/option[1108]')
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(approval_from)).click()
+    purpose = (By.XPATH, '//*[@id="normal_stayback"]/form/fieldset/section[1]/div/label[2]/i') # fills in the form
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(purpose)).click()
 
-date = (By.NAME, "stayback_date")
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(date)).send_keys(datetime.today().strftime('%Y-%m-%d'))
+    approval_from = (By.XPATH, '//*[@id="staff_id"]/option[1108]')
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(approval_from)).click()
 
-lt = (By.ID, "clockpicker1")
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(lt)).send_keys("10:30")
+    date = (By.NAME, "stayback_date")
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(date)).send_keys(datetime.today().strftime('%Y-%m-%d'))
 
-at = (By.ID, "clockpicker2")
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(at)).send_keys("11:00")
+    lt = (By.ID, "clockpicker1")
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(lt)).send_keys("10:30")
 
-reason = (By.NAME, "reason")
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(reason)).click()
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(reason)).send_keys("amfoss")
+    at = (By.ID, "clockpicker2")
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(at)).send_keys("11:00")
 
-submit = (By.ID, "stayback_normal_save")
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(submit)).click()
+    reason = (By.NAME, "reason")
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(reason)).click()
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(reason)).send_keys("amfoss")
 
-#browser.__exit__()
+    submit = (By.ID, "stayback_normal_save") # clicks on the submit button
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable(submit)).click()
+
